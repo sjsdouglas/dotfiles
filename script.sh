161391f0bc993a3eb42258c0bee22d5de6817a23
sjsDouglas <104078248+sjsdouglas@users.noreply.github.com>
2022-06-29 16:01:32 -0300

    atualização

--- script.sh
+++ script.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-#Install GIT, GO, ZSH, CURL
+#Instala o GIT, GO, ZSH, CURL
 sudo pacman -S --needed git go zsh curl
 
-#Change Shell
+#Muda o shell
 chsh -s $(which zsh)
 
 #Log-off
 #Log-in
 
-#Install OhMyZsh
+#Instala o OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-### Setup dos dotfiles - clona e copia arquivos
+#Setup dos dotfiles - clona e linka os arquivos
 git clone https://github.com/sjsdouglas/dotfiles ~/.dotfiles
 cd ~/.dotfiles
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.config/i3/config ~/.config/i3/config
+ln -s ~/.dotfiles/.config/picom/picom.conf ~/.config/picom/picom.conf
+ln -s ~/.dotfiles/.config/polybar/... ~/.config/polybar/...
